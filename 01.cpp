ce6bff10bc1bbfef363a67eab9db73cfba68c737
Dmitry Barskov <barskoidim@gmail.com>
2019-10-06 23:34:57 +0300

    add thread ids output for 01

--- 01.cpp
+++ 01.cpp
@@ -11,6 +11,8 @@ int main() {
     omp_set_num_threads(8);
     #pragma omp parallel
     {
-        printf("Hello World\n");
+        printf("T%d/%d: Hello World\n",
+               omp_get_thread_num(),
+               omp_get_num_threads());
     }
 }
